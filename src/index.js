8b6421c20d510b5187afa68bc8c4fb29a1dac9e5
Charles Rathaba <rathabacharles@gmail.com>
2024-11-13 21:27:58 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// The entry point for the app.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
